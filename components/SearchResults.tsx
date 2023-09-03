18f0b404ef248502417f10a98871acfbb4ab35f0
Gustavo Priebe <gu181288@gmail.com>
2023-09-03 01:47:55 -0300

    ts minor bug fix

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,27 +1,23 @@
 import axios from "axios";
 
-interface product {
+interface Product {
   id: number;
   name: string;
   price: number;
 }
 
-interface products {
-  map: any;
-  data: product[];
+interface Products {
+  data: Product[];
 }
 
 export default async function SearchResults() {
-  const res = await axios.get("http://localhost:3333/products");
-
-  const products: products = res.data;
-
-  console.log(products);
+  const res = await axios.get<Products>("http://localhost:3333/products");
+  const products: Product[] = res.data.data;
 
   return (
     <div>
       <h1>Search Results</h1>
-      {products.map((product: product) => {
+      {products.map((product: Product) => {
         return <p key={product.id}>{product.name}</p>;
       })}
     </div>
